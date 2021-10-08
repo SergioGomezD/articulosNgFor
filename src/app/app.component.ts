1a44d8a33b8cac7b8e54953d4036471ac6236f43
SgD200 <sergiogomdu@hotmail.com>
2021-10-08 11:24:38 -0500

    version final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   codigo:number = 0;
   descripcion:string = '';
   precio:number = 0;
-  articuloSave:number=0;
+  articuloSave:number=100;
 
 
   articulos:Articulo[] = [new Articulo(1,'Jiménez',0)];
@@ -20,24 +20,29 @@ export class AppComponent {
   agregarArticulo(){
     let articulo1=new Articulo(this.codigo, this.descripcion, this.precio);
     this.articulos.push(articulo1);
+    this.codigo=0;
+    this.descripcion='';
+    this.precio=0;
+
   }
   modificarArticulo(){
     this.articulos[this.articuloSave]= new Articulo(this.codigo, this.descripcion, this.precio);
+    this.codigo=0;
+    this.descripcion='';
+    this.precio=0;
   }
+
+
   eliminarArticulo(art:number){
     this.articulos.splice(art,1);
     console.log(art);
 
   }
   guardarArticulo(art:number){
-    const codigoF = document.getElementById("codigoI");
-    const descripcionF = document.getElementById("descripcionI");
-    const precioF = document.getElementById("precioI");
     this.articuloSave=art;
     console.log(this.articuloSave);
-    //codigoF.=this.articulos[this.articuloSave].codigo;
-    //this.articulos[this.articuloSave].descripcion
-    //this.articulos[this.articuloSave].precio
-
+    this.codigo=this.articulos[this.articuloSave].codigo;
+    this.descripcion=this.articulos[this.articuloSave].descripcion;
+    this.precio=this.articulos[this.articuloSave].precio;
   }
 }
