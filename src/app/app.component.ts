2e1c69f5a9bf02d62c43a0707084e5f034a24db4
SgD200 <sergiogomdu@hotmail.com>
2021-10-08 08:59:22 -0500

    modificar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   codigo:number = 0;
   descripcion:string = '';
   precio:number = 0;
-  art:number=0;
+  articuloSave:number=0;
 
 
   articulos:Articulo[] = [new Articulo(1,'Jiménez',0)];
@@ -22,11 +22,22 @@ export class AppComponent {
     this.articulos.push(articulo1);
   }
   modificarArticulo(){
-
+    this.articulos[this.articuloSave]= new Articulo(this.codigo, this.descripcion, this.precio);
   }
   eliminarArticulo(art:number){
     this.articulos.splice(art,1);
     console.log(art);
+
+  }
+  guardarArticulo(art:number){
+    const codigoF = document.getElementById("codigoI");
+    const descripcionF = document.getElementById("descripcionI");
+    const precioF = document.getElementById("precioI");
+    this.articuloSave=art;
+    console.log(this.articuloSave);
+    //codigoF.=this.articulos[this.articuloSave].codigo;
+    //this.articulos[this.articuloSave].descripcion
+    //this.articulos[this.articuloSave].precio
 
   }
 }
